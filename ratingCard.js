6e1573838d67a983bf9bd261e34f4da9eea3a1c4
rnasante <raynice54@gmail.com>
2022-09-09 13:56:53 +0100

    HTML, CSS and JavaScript for interactive rating component

--- ratingCard.js
+++ ratingCard.js
@@ -1,3 +1,17 @@
-function getThankYou () {
-    window.location = "thankYou.html";
-}
+const rateContainer = document.querySelector(".container");
+const thankYouContainer = document.querySelector(".thank-you")
+const submitButton = document.getElementById("btn")
+
+submitButton.addEventListener("click", () => {
+    thankYouContainer.classList.remove("hidden")
+    rateContainer.style.display = "none"
+})
+
+const rateValues = document.getElementById("rating-value")
+const rateOptions = document.querySelectorAll(".dot")
+
+rateOptions.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rateValues.innerHTML = rate.innerHTML
+    })
+})
