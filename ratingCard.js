1f02bd8cf814f4e770084566cf150e1fe9a4fea0
rnasante <raynice54@gmail.com>
2022-09-09 22:10:25 +0100

    final changes

--- ratingCard.js
+++ ratingCard.js
@@ -2,10 +2,11 @@ const rateContainer = document.querySelector(".container");
 const thankYouContainer = document.querySelector(".thank-you")
 const submitButton = document.getElementById("btn")
 
-submitButton.addEventListener("click", () => {
+if(submitButton) {
+    submitButton.addEventListener("click", () => {
     thankYouContainer.classList.remove("hidden")
     rateContainer.style.display = "none"
-})
+})}
 
 const rateValues = document.getElementById("rating-value")
 const rateOptions = document.querySelectorAll(".dot")
